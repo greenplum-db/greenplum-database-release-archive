67071f72789304b8d9ea569d2da85363bff49b7b
bhanu-atturu <79278246+bhanu-atturu@users.noreply.github.com>
2021-10-13 15:04:12 -0400

    Add yes flag to dnf update on rhel8

--- ci/concourse/scripts/test-package-dependencies.bash
+++ ci/concourse/scripts/test-package-dependencies.bash
@@ -14,7 +14,7 @@ if [[ $PLATFORM == "rhel"* ]]; then
 	fi
 
 	if [[ $PLATFORM == "rhel8" ]]; then
-		dnf update && dnf install -y subscription-manager
+		dnf update -y && dnf install -y subscription-manager
 		subscription-manager register --org=${REDHAT_SUBSCRIPTION_ORG_ID} --activationkey=${REDHAT_SUBSCRIPTION_KEY_ID}
 		subscription-manager attach --auto
 	fi
