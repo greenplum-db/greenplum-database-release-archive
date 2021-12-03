85208dcddd0fdf8a9d160cf6113aefc56de47b09
Ning Wu <ningw@vmware.com>
2021-12-03 14:57:35 +0800

    Remove triger by gpdb_src
    Pipelines are now triggered by pivotal_gpdb
    
    [GPR-593]
    
    Co-authored-by: Ning Wu <ningw@vmware.com>
    Co-authored-by: Shaoqi Bai <bshaoqi@vmware.com>

--- ci/concourse/scripts/push-to-tanzunet.bash
+++ ci/concourse/scripts/push-to-tanzunet.bash
@@ -3,11 +3,10 @@
 push() {
 	set -ex
 
-	cd gpdb_src
-	GPDB_VERSION=$(./getversion --short)
+	GPDB_VERSION=$(cat pivotal-gpdb/version | cut -d "#" -f 1)
 
-	chmod a+x ../tanzunet_client/gp-tanzunet-client
-	../tanzunet_client/gp-tanzunet-client upload --verbose --parent-tanzunet-slug vmware-tanzu-greenplum --metadata "../greenplum-database-release/${TANZUNET_METADATA_FILE}" --search-path ../ --gpdb-version "${GPDB_VERSION}" --debug
+	chmod a+x ./tanzunet_client/gp-tanzunet-client
+	./tanzunet_client/gp-tanzunet-client upload --verbose --parent-tanzunet-slug vmware-tanzu-greenplum --metadata "../greenplum-database-release/${TANZUNET_METADATA_FILE}" --search-path ../ --gpdb-version "${GPDB_VERSION}" --debug
 }
 
 if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
