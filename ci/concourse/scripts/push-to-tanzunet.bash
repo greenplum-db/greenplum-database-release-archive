6473a5a72623c416ceb6252af2a197863de347cc
Ning Wu <ningw@vmware.com>
2021-12-06 11:51:50 +0800

    Add dry run mode for push to tanzunet job
    Co-authored-by: Ning Wu <ningw@vmware.com>
    Co-authored-by: Shaoqi Bai <bshaoqi@vmware.com>

--- ci/concourse/scripts/push-to-tanzunet.bash
+++ ci/concourse/scripts/push-to-tanzunet.bash
@@ -6,7 +6,11 @@ push() {
 	GPDB_VERSION=$(cat pivotal-gpdb/version | cut -d "#" -f 1)
 
 	chmod a+x ./tanzunet_client/gp-tanzunet-client
-	./tanzunet_client/gp-tanzunet-client upload --verbose --parent-tanzunet-slug vmware-tanzu-greenplum --metadata "../greenplum-database-release/${TANZUNET_METADATA_FILE}" --search-path ../ --gpdb-version "${GPDB_VERSION}" --debug
+	if [ "${RUN_MODE}" != "prod" ]; then
+		./tanzunet_client/gp-tanzunet-client upload --verbose --parent-tanzunet-slug vmware-tanzu-greenplum --metadata "./greenplum-database-release/${TANZUNET_METADATA_FILE}" --search-path ./ --gpdb-version "${GPDB_VERSION}" --debug --dryrun
+	else
+		./tanzunet_client/gp-tanzunet-client upload --verbose --parent-tanzunet-slug vmware-tanzu-greenplum --metadata "./greenplum-database-release/${TANZUNET_METADATA_FILE}" --search-path ./ --gpdb-version "${GPDB_VERSION}" --debug
+	fi
 }
 
 if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
